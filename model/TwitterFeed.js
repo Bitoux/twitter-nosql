5bb4e879dca88ea70746f2fe50faf26d051e2449
Unknown <yann.miloux@gmail.com>
2018-06-06 16:36:53 +0200

    Twitter // Get NBAFinals tweets with Twitter API

---
+++ model/TwitterFeed.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose'); // Import de la librairie mongoose
+const Schema = mongoose.Schema;
+// Définition du schéma
+const TwitterFeed = new Schema({
+    name: {type: String, required: true},
+    status: {type: String, enum: ['todo', 'inProgress', 'done'], default: 'todo', required: true}
+  }, 
+  {timestamps: true} // Pour avoir les dates de création et de modification automatiquement gérés par mongoose
+);
+module.exports = TodoItemSchema; // Export du schéma
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,28 @@
+var Twitter = require('twitter');
+
 var appRouter = function (app) {
     app.get("/", function(req, res) {
         res.status(200).send("Welcome to our restful API");
+    });
+
+    app.get("/tweets", function(req, res){
+        require('dotenv').load();
+
+        var client = new Twitter({
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        });
+
+        client.get('search/tweets', {q: 'NBAFinals'})
+        .then(function (tweet) {
+            console.log(tweet);
+            res.status(200).send(tweet);
+        })
+        .catch(function (error) {
+            throw error;
+        });
     });
 }
   
