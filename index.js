4d8ba9d3404e77da51d9600e19217f998e1dcd10
Unknown <yann.miloux@gmail.com>
2018-06-07 10:34:17 +0200

    WIP // Mongo

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var routes = require("./routes/routes.js");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const routes = require("./routes/routes.js");
+const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/TodoListDB');
+
+
 routes(app);
 
-var server = app.listen(3000, function () {
+const server = app.listen(3000, function () {
     console.log("app running on port. ->", server.address().port);
 });
--- model/TwitterFeed.js
+++ model/TwitterFeed.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose'); // Import de la librairie mongoose
 const Schema = mongoose.Schema;
 // Définition du schéma
 const TwitterFeed = new Schema({
-    name: {type: String, required: true},
-    status: {type: String, enum: ['todo', 'inProgress', 'done'], default: 'todo', required: true}
+    id: {type: Number, required: true},
+    created_at: {type: String, required: true},
+    retweet_count: {type: Number, required: false},
+    favorite_count: {type: Number, required: false},
+    followers_count: {type: Number, required: false},
+    friends_count: {type: Number, required: false}
   }, 
   {timestamps: true} // Pour avoir les dates de création et de modification automatiquement gérés par mongoose
 );
